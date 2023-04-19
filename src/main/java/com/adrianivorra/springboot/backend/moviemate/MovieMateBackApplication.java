9e54a26c22e4601a1d3ce68aea41a44e23de1ada
Adrian Ivorra <adrianivorraaracil28@gmail.com>
2023-04-19 12:13:58 +0200

    update

--- src/main/java/com/adrianivorra/springboot/backend/moviemate/MovieMateBackApplication.java
+++ src/main/java/com/adrianivorra/springboot/backend/moviemate/MovieMateBackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieMateBackApplication {
-//rgt
+//rgtgf
 	public static void main(String[] args) {
 		SpringApplication.run(MovieMateBackApplication.class, args);
 	}
